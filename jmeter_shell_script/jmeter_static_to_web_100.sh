ba74099fc07c1112b4b40beabab74c2adcef25ec
root <maladroit1@naver.com>
2019-07-31 09:17:22 +0900

    update_jmeter_shell_script

---
+++ jmeter_shell_script/jmeter_static_to_web_100.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+cd ~/object_storage
+aws --endpoint-url=http://kr.object.ncloudstorage.com s3 sync s3://bucket-asset .
+cd ~/apache-jmeter-5.1.1/bin
+rm -rf asdf.jtl
+./jmeter -n -t ~/object_storage/final_100.jmx -l asdf.jtl
+rm -rf /usr/share/nginx/html/result
+./jmeter -g asdf.jtl -f -o /usr/share/nginx/html/result
